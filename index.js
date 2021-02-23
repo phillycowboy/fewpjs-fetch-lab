80f6c392520ad1eb3e3b82603b0651f8ec842012
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-23 13:02:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => {
+      renderBooks(data);
+    });
 }
 
 function renderBooks(books) {
